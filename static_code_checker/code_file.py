2af2e9e9af097abd8f6ff30f89e05f16be53144f
Jake Gadaleta <jake.gads@gmail.com>
2020-05-06 23:50:35 -0400

    Syncing

--- static_code_checker/code_file.py
+++ static_code_checker/code_file.py
@@ -23,14 +23,41 @@ class File():
         self.outputs = outputs
 
     def run_tests(self):
-        if not self.cleared:
-            return False
+        value = "<div>"
+        for i in range(self.inputs):
+            # file, args: list = None
+            value += compare(self.languageType.run_file(self.file_loc, self.inputs[i]), self.outputs[i])
+            value += "</div>"
+            
+def compare(original_text: str, generated_text: str):
+    """
+    Compares 2 strings and returns html to be served to the models
+    original_text - The text from the example
+    generated_text - The text from the
+    """
+    def prep_html(text: str, expression: bool):
+        """
+        Returns the match or miss for each character while already moving it to html
+        text: the actual character
+        expression: matching status
+        """
+        if expression:
+            return f'<div class="__g__"> {text} </div>'
+        else:
+            return f'<div class="__r__"> {text} </div>'
 
-        full_text = ""
-        reusable = "<div><p>ChangeThis<\p></div>"
+    comparison = ""
+    
+    # we will assume that this is the actual text
+    for i in range(len(generated_text)):
+        try:
+            comparison += prep_html(generated_text[i], generated_text[i] == original_text[i])
+        except IndexError:
+            comparison += prep_html(generated_text[i], False)
 
-        for i in range(inputs):
-            full_text += reusable.replace(
-                "ChangeThis",  self.languageType.run_file(self.file_loc, self.inputs[i]))
-        
-        return full_text        
+    if len(original_text) > len(generated_text):
+        comparison += prep_html(original_text[len(generated_text):len(original_text)], False)
+
+    percent_match = comparison.count("__g__") / (comparison.count("__g__") + comparison.count("__r__"))
+
+    return comparison, percent_match
--- static_code_checker/compiler.py
+++ static_code_checker/compiler.py
@@ -6,19 +6,39 @@ try:
     from code_file import  File
 except :
     from static_code_checker.code_file import File
-
-def file_parser(line):
-    # is a file
-    if os._exists(line):
-        contents = ''
-        with open(line, 'r') as reader:
-            for i in reader.readlines():
-                contents += i
-        contents.split('^^^')
-        return contents
-    else:
-        return line
         
+
+def get_and_clean_files():
+    def file_parser(line):
+        # is a file
+        if os._exists(line):
+            contents = ''
+            with open(line, 'r') as reader:
+                for i in reader.readlines():
+                    contents += i
+            contents.split('^^^')
+            return contents
+        else:
+            return line
+
+    files = []
+    io_files = sys.argv
+    for i in range(io_files):
+        if i > 1:
+            break
+        io_files[i] = file_parser(io_files[i])
+
+
+    for r, d, f in os.walk(os.getcwd()):
+        for file in f:
+            files.append(os.path.join(r, file), *io_files)
+
+    for i in range(files):
+        if not files[i].cleared:
+            del files[i]
+        if io_files[2]:
+            if 'driver' not in file[i].file_loc:
+                del files[i]
 
 startHTML = """
 <head>
@@ -40,26 +60,7 @@ endHTML = """
 </body>
 """
 
-files = []
-io_files = sys.argv
-for i in range(io_files):
-    if i > 1:
-        break
-    io_files[i] = file_parser(io_files[i])
-
-
-for r, d, f in os.walk(os.getcwd()):
-    for file in f:
-        files.append(os.path.join(r, file), *io_files)
-        
-for i in range(files):
-    if not files[i].cleared:
-        del files[i]
-    if io_files[2]:
-        if 'driver' not in file[i].file_loc:
-            del files[i]
-    
-
+files = get_and_clean_files()
 
 
 
